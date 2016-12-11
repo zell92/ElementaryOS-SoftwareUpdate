1f23b5cba19353c56eb8c0ade60c256eb55b6ff1
zell92 <n.sardella@hotmail.it>
2016-12-11 19:10:39 +0100

    Update bestSoftware.sh

--- bestSoftware.sh
+++ bestSoftware.sh
@@ -4,6 +4,7 @@ RED='\033[0;31m'
 NC='\033[0m' # No Color
 set -e
 echo  -e "${RED}System Update:${NC}"
+sudo apt-get update
 yes y | sudo apt install software-properties-gtk software-properties-common
 yes y | sudo apt-get update && sudo apt-get dist-upgrade
 yes y | sudo apt-get install ubuntu-restricted-extras p7zip gdebi synaptic menulibre dconf-tools
